f891acb9648165bc0d1e519091d702bae2bc2194
Dan Thick <dan.thick@hotmail.co.uk>
2020-10-07 19:10:07 +0100

    2nd commit

--- index.js
+++ index.js
@@ -5,12 +5,21 @@ const puppeteer = require('puppeteer');
         headless: false
     });
 
+    // Open webpage
     const page = await browser.newPage();
     await page.goto('https://bookings.ledleisure.co.uk/mrmlogin.aspx');
-    console.log('Page loaded');
+    
+    // Enter username
     await page.click('#ctl00_MainContent_InputLogin');
-    await page.type('#ctl00_MainContent_InputLogin', "N0077798", {delay: 1})
+    await page.type('#ctl00_MainContent_InputLogin', "N0077798")
+
+    // Enter password
     await page.click('#ctl00_MainContent_InputPassword');
     await page.type('#ctl00_MainContent_InputPassword', "1125")
+
+    // Click login
     await page.click('#ctl00_MainContent_btnLogin');
+
+    await page.click('#ctl00_MainContent__advanceSearchUserControl__lnkBtnSevenDaysTime');
+
   })().catch( e => { console.error(e) })
