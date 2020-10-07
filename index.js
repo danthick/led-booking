779dbb2c78584b58b6ee998b16c25deaf034838a
Dan Thick <dan.thick@hotmail.co.uk>
2020-10-07 22:18:06 +0100

    Implemented login and accessing the last page of classes. Need to work on checking each class time to get the right one.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer');
+var page = null;
 
 (async function () {
     const browser = await puppeteer.launch({
@@ -6,7 +7,7 @@ const puppeteer = require('puppeteer');
     });
 
     // Open webpage
-    const page = await browser.newPage();
+    page = await browser.newPage();
     await page.goto('https://bookings.ledleisure.co.uk/mrmlogin.aspx');
     
     // Enter username
@@ -19,7 +20,35 @@ const puppeteer = require('puppeteer');
 
     // Click login
     await page.click('#ctl00_MainContent_btnLogin');
+    await page.waitFor(2000);
 
-    await page.click('#ctl00_MainContent__advanceSearchUserControl__lnkBtnSevenDaysTime');
+    // Click '7 days time'
+    await page.click('#ctl00_MainContent__advanceSearchUserControl__lnkBtnSevenDaysTime', {timeout: 50000});
 
-  })().catch( e => { console.error(e) })
+    await page.waitFor(2000);
+
+    // Query parameters
+    const exerciseClass = "Body Balance";
+    const time = "10:30";
+    var targetElements;
+
+    targetElements = await page.evaluate(exerciseClass => {
+        // Get all elements
+        const elements = [...document.querySelectorAll('a')];
+
+        // Filter to find elements with matching inner text
+        var e = elements.filter(e => e.innerText.includes(exerciseClass));
+        return e;
+
+        // Make sure the element exists, and only then click it
+        //targetElements && targetElements[0].click();
+    }, exerciseClass)
+
+    for (let element of targetElements) {
+        console.log(element);
+        // element.click();
+        // await page.waitFor(2000);
+        // page.goBack();
+    }
+
+})().catch( e => { console.error(e) })
